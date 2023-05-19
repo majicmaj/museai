b6440d2d8eb48ba0ee05b819890cb8921de749b5
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2023-05-19 19:44:06 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Muse AI</h1>
+        <input className="input" autoFocus placeholder="What's the vibe?" />
+        <button>Compose</button>
+      </div>
+    </div>
+  );
+}
